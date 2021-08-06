decf4470c5481c8ff6846fb78229b26f1a617265
amira <amiramahmoudabd@gmail.com>
2021-08-06 22:44:59 +0200

    first commit

---
+++ amira.jl
@@ -0,0 +1,18 @@
+# a script that prints my name, slack username, biostack
+
+name = "Amira Mahmoud"
+email = "amiramahmoudabd@gmail.com"
+slack = "@amira"
+biostack = "genomics"
+twitter_handle = "@AmiraMa25428958"
+
+for i in [name, email, slack, biostack, twitter_handle]
+	println(i)
+end
+
+
+#solving the hamming_distance
+
+println(cmp(slack, twitter_handle))
+
+
