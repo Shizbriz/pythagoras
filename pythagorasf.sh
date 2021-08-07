5678e454acc242d0f4b9cb48d4cfe53cc2e83bb7
amiraa25 <62318888+amiraa25@users.noreply.github.com>
2021-08-07 18:49:15 +0200

    Add files via upload

---
+++ pythagorasf.sh
@@ -0,0 +1,37 @@
+git clone https://github.com/Shizbriz/pythagoras.git
+
+output = pythagoras_members.csv
+
+cd ./pythagoras
+
+chmod +x script*
+ls script*
+
+for i in $(ls script*)
+do
+	if [[ $i == *.py ]]
+	then
+		python3 $i >> pythagoras_members.csv
+	elif [[ $i == *.js ]]
+	then
+		node $i >> pythagoras_members.csv
+	elif [[ $i == *.cpp ]]
+	then
+		g++ $i >> pythagoras_members.csv
+	elif [[ $i == *.jl ]]
+	then
+		julia $i >> pythagoras_members.csv
+	elif [[ $i == *.pl ]]
+	then
+		perl $i >> pythagoras_members.csv
+	elif [[ $i == *.R ]]
+	then
+		Rscript $i >> pythagoras_members.csv
+	elif [[ $i == *.M ]]
+	then
+		matlab $i >> pythagoras_members.csv
+	else 
+		scilab $i >> pythagoras_members.csv
+	fi
+done
+
